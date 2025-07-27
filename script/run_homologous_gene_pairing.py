c21cc8ce1f852f83428830ee9fa80b31a3b4e3d2
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-07-27 22:39:14 +0900

    add homologous pairing

---
+++ script/run_homologous_gene_pairing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+pd.set_option('display.max_colwidth', None)  
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
+
+mice_annotated_exons = pd.read_pickle('./data/output/mm39_annotated_exons.pickle')
+human_annotated_exons = pd.read_pickle('./data/output/hg38_annotated_exons.pickle')
+
+homologous_df = pd.read_csv('data/homologous_pair/homologous_pair.txt', sep=',', header = 0)
+homologous_df.columns = homologous_df.columns.str.strip()
+print(homologous_df.columns)
+
+print(len(homologous_df)) #92566
+print(homologous_df["Gene stable ID"].duplicated().sum()) #6232
+
+print(mice_annotated_exons.head())
+print(mice_annotated_exons.columns)
+print(human_annotated_exons.head())
+print(human_annotated_exons.columns)
--- src/homologous_gene_pairing.py
+++ src/homologous_gene_pairing.py
@@ -3,8 +3,20 @@ import pandas as pd
 def extract_genename_as_column(annotated_exons):
     """
     Pourpose: attribute列に複数の情報が格納されているので、gene nameを抽出して新しい列に格納する
+    input: annotated_exons (DataFrame): アノテーションされたエキソンのデータ
+    output: annotated_exons (DataFrame): gene nameを含む新しい列を追加したDataFrame
     """
     # Extract gene names from the 9th column (attributes)
+    annotated_exons['gene_id'] = annotated_exons[8].str.extract(r'gene_id "([^"]+)"')
     annotated_exons['gene_name'] = annotated_exons[8].str.extract(r'gene_name "([^"]+)"')
-
+    annotated_exons['transcript_id'] = annotated_exons[8].str.extract(r'transcript_id "([^"]+)"')
+    annotated_exons['transcript_biotype'] = annotated_exons[8].str.extract(r'transcript_biotype "([^"]+)"')
+    annotated_exons['exon_id'] = annotated_exons[8].str.extract(r'exon_id "([^"]+)"')
+    annotated_exons['exon_number'] = annotated_exons[8].str.extract(r'exon_number "([^"]+)"')
+    annotated_exons.drop(columns=[8], inplace=True)
     return annotated_exons
+
+def make_homologous_gene_dict(homologous_df):
+    """
+    Pourpose: homologous_dfからhg38のgene_idをキー、mm39のgene_idをvalueとする辞書を作成する
+    """
