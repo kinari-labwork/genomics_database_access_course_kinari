0af8c60104668687b0fc1c136c9db845348dce1c
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-07-27 18:50:42 +0900

    毎回fasta object を生成しないように変更

--- src/sequence_anotator.py
+++ src/sequence_anotator.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from pyfaidx import Fasta
 
+def covert_sequence_to_reverse_complement(sequence: str) -> str:
+    """
+    purpose: 塩基配列を逆相補鎖に変換する
+    input: sequence (str): 塩基配列
+    output: reverse_complement (str): 逆相補鎖の塩基配列
+    """
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'N': 'N'}  # Nは不明な塩基
+    reverse_complement = ''.join(complement[base] for base in reversed(sequence))
+    return reverse_complement
+
+
+
 def extract_exons(gtf_file: pd.DataFrame) -> pd.DataFrame:
     """
     purpose: GTFファイルからエキソン情報を抽出し、重複を除去する
@@ -15,7 +27,7 @@ def extract_exons(gtf_file: pd.DataFrame) -> pd.DataFrame:
     # カラム名を設定
     return exon_data
 
-def get_exon_sequences(row: pd.Series, fasta_file: str) -> str:
+def get_exon_sequences(row: pd.Series, fasta_obj: Fasta) -> str:
     """
     purpose: エキソンの開始位置と終了位置を基に、FASTAファイルからエキソンのシーケンスを取得する
     input: row (Series): エキソンの行データ
@@ -27,11 +39,10 @@ def get_exon_sequences(row: pd.Series, fasta_file: str) -> str:
     chrom = row[0]
     
     # FASTAファイルからエキソンのシーケンスを取得
-    fasta = Fasta(fasta_file)
-    sequence = fasta[chrom][start:end].seq
+    sequence = str(fasta_obj[chrom][start:end])
     if row[6] == '-':
-        sequence = sequence.reverse.complement()
-    return str(sequence)
+        sequence = covert_sequence_to_reverse_complement(sequence)
+    return sequence
 
 def annotate_exons_with_sequences(exon_data: pd.DataFrame, fasta_file: str) -> pd.DataFrame:
     """
@@ -39,6 +50,7 @@ def annotate_exons_with_sequences(exon_data: pd.DataFrame, fasta_file: str) -> p
     input: exon_data (DataFrame): エキソン情報を含むDataFrame
            fasta_file (str): FASTAファイルのパス
     """
-    exon_data['sequence'] = exon_data.apply(lambda row: get_exon_sequences(row, fasta_file), axis=1)
+    fasta = Fasta(fasta_file)
+    exon_data['sequence'] = exon_data.apply(lambda row: get_exon_sequences(row, fasta), axis=1)
     return exon_data
 
